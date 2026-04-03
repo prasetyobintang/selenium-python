035f4d4d458b4e4baedc698b2766eb457b34835e
Bintang <prasetyobintangg@gmail.com>
2026-04-03 09:01:24 +0700

    fix: correct typo

--- tests/positive/saucedemo_positive.py
+++ tests/positive/saucedemo_positive.py
@@ -1,7 +1,6 @@
 # import lib/dependecy yang dibutuhkan
 from modulefinder import test
 from faker import Faker
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
